a96127f164b56859657e163b7df3a534d05261bb
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2021-11-27 19:40:46 +0100

    Assignment update

--- src/assignments/Decrypt.java
+++ src/assignments/Decrypt.java
@@ -1,5 +1,7 @@
 package assignments;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
 public class Decrypt {
@@ -7,6 +9,46 @@ public class Decrypt {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Encrypted Data: ");
         int encrypted = scan.nextInt();
+
+        int firstDigit = encrypted / 1000;
+        int secondDigit = (encrypted % 1000)/100;
+        int thirdDigit = (encrypted % 100)/10;
+        int fourthDigit = (encrypted % 10)/1;
+
+        if (firstDigit >= 7){
+          firstDigit =   firstDigit - 7;
+        }
+        else {
+            firstDigit =  (firstDigit + 10) - 7;
+        }
+
+        if (secondDigit >= 7){
+            secondDigit = secondDigit - 7;
+        }
+        else {
+            secondDigit = (secondDigit + 10) - 7;
+        }
+
+        if (thirdDigit >= 7){
+            thirdDigit = thirdDigit - 7;
+        }
+        else {
+            thirdDigit = (thirdDigit + 10) - 7;
+
+        }
+
+        if (fourthDigit >= 7){
+            fourthDigit = fourthDigit - 7;
+
+        }
+        else {
+            fourthDigit = (fourthDigit + 10) - 7;
+
+        }
+
+        System.out.printf("The decrypted data is %d%d%d%d",thirdDigit,fourthDigit,firstDigit,secondDigit);
+
+
 
 
     }
