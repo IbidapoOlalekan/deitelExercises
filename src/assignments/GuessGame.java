0e71ff42387077db228b2664f16d85f9fb007412
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2021-11-27 20:28:57 +0100

    Assignment update

---
+++ src/assignments/GuessGame.java
@@ -0,0 +1,35 @@
+package assignments;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class GuessGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count;
+
+        SecureRandom numbers = new SecureRandom();
+        int min = 1;
+        int max = 1000;
+        int guess = numbers.nextInt(1000);
+        int numberOfTries = 0;
+        int num;
+        do {
+            System.out.println("Guess a number between 1 and 1000");
+             num = scan.nextInt();
+            numberOfTries++;
+
+            if (num < guess) {
+                System.out.println("Too Low.. Try Again!!!!!!");
+            } else {
+                if (num == guess) {
+                    System.out.println("Congratulations, You guessed the number!!!");
+                } else {
+                    System.out.println("Too High... Try Again!!!!!");
+                }
+            }
+        }
+        while (num != guess);
+
+    }
+}
