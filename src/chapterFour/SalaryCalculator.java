bc1cc5f8787f9cb6395cdf668bb41491b8702fee
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2021-11-30 14:00:09 +0100

    Assignment update

--- src/chapterFour/SalaryCalculator.java
+++ src/chapterFour/SalaryCalculator.java
@@ -1,7 +1,30 @@
 package chapterFour;
 
+import java.util.Scanner;
+
 public class SalaryCalculator {
     public static void main(String[] args) {
+        // Get the employee details
+        //
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the employee name: ");
+        String name = scan.nextLine();
+        System.out.println("Enter the hours : ");
+        int hours = scan.nextInt();
+        System.out.println("Enter the hourly rate: ");
+        double hourlyRate = scan.nextDouble();
+
+            double grossPay = hours * hourlyRate;
+            System.out.printf("%s gross pay is : %.2f%n",name,grossPay);
+
+            if (hours > 40){
+                hours =  hours - 40;
+                double newGross = hours * hourlyRate * 1.5;
+
+                grossPay = grossPay + newGross;
+
+                System.out.printf("%s gross pay is : %.2f",name,grossPay);
+            }
 
     }
 }
--- src/chapterSix/Factors.java
+++ src/chapterSix/Factors.java
@@ -1,30 +1,35 @@
 package chapterSix;
 
+import java.util.IllegalFormatCodePointException;
 import java.util.Scanner;
 
 public class Factors {
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int number = scan.nextInt();
-
-
-        int count = 0;
-        System.out.print("The factors are ");
-        for (int root=1; root <= number; root++ ){
-
-          if (number % root == 0){
-
-              System.out.print(root + " ");
-
-              //System.out.printf("The factors are %d%n", root);
-              count++;
-          }
-
-
+    public static int calculateFactorsOf(int userInput){
+        int factors = 0;
+        for (int number = 1; number <= userInput; number++){
+            if (userInput % number == 0){
+                factors++;
+            }
         }
-        System.out.println("");
-        System.out.printf("The number of factors are %d",count);
+        return factors;
     }
+    public static void main(String[] args) {
+        calculatePrimeNumbers();
+
+    }
+    public static void calculatePrimeNumbers(){
+        System.out.println("Enter a number to check for: ");
+        Scanner scan = new Scanner(System.in);
+        int userInput = scan.nextInt();
+
+        int result  = calculateFactorsOf(userInput);
+
+        if (result == 2){
+            System.out.printf("%d is a Prime number%n",userInput);
+        }
+        else {
+            System.out.printf("%d is not a prime number%n",userInput);
+        }
+    }
+
 }
