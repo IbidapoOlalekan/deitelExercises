e4d8c2642860128b404a06e81f7c03314f7a71c6
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2021-11-30 14:21:59 +0100

    Chapter 4 update

---
+++ src/chapterFour/TabularOutput.java
@@ -0,0 +1,15 @@
+package chapterFour;
+
+public class TabularOutput {
+    public static void main(String[] args) {
+        for (int n = 1; n < 6; n++ ){
+            System.out.print(n + " ");
+            System.out.print(n * 10 + " ");
+            System.out.print(n * 100 + " ");
+            System.out.print(n * 1000+ " ");
+            System.out.println(" ");
+
+
+        }
+    }
+}
