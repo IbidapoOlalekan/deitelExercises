cbb7084899d35b71f39e5d40005e60e999f7f0cb
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2021-12-12 01:07:32 +0100

    Updated Practice Code

--- test/practiceDeitel/MonthSwitch.java
+++ test/practiceDeitel/MonthSwitch.java
@@ -16,25 +16,10 @@ public class MonthSwitch {
         Scanner scan = new Scanner(System.in);
         String month = scan.next();
 
-        switch (month){
-            case "January":
-            case "March":
-            case "May":
-            case "July":
-            case "August":
-            case "October":
-            case "December":
-                System.out.println("It have 31 days");
-                break;
-            case "February":
-                System.out.println("It have 28 days");
-                break;
-            case "April":
-            case "June":
-            case "September":
-            case "November" :
-                System.out.println("It have 30 days");
-                break;
+        switch (month) {
+            case "January", "March", "May", "July", "August", "October", "December" -> System.out.println("It have 31 days");
+            case "February" -> System.out.println("It have 28 days");
+            case "April", "June", "September", "November" -> System.out.println("It have 30 days");
         }
     }
 }
