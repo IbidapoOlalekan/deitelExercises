d85fa998522f6318baaefe1723150ee4f92c6840
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2022-01-10 17:35:25 +0100

    Updated  Code

--- src/chapterSix/DistanceBetweenPoints.java
+++ src/chapterSix/DistanceBetweenPoints.java
@@ -6,19 +6,24 @@ public class DistanceBetweenPoints {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-       print("Enter the coordinates: ");
-       double coordinateOne = scan.nextDouble();
-       print("Enter the coordinates: ");
-       double coordinateTwo = scan.nextDouble();
+       print("Enter the X coordinates: ");
+       double xOne = scan.nextDouble();
+       print("Enter the X coordinates: ");
+       double xTwo = scan.nextDouble();
+       print("Enter the Y coordinates: ");
+       double yOne = scan.nextDouble();
+       print("Enter the Y Coordinates: ");
+       double yTwo = scan.nextDouble();
 
-      double distance =  distance(coordinateOne,coordinateTwo);
-
+      double distance =  distance(xOne,yOne,xTwo,yTwo);
       print("The distance between the two coordinates is " + distance);
 
     }
 
-    public static double distance(double pointOne, double pointTwo){
-        return pointTwo - pointOne;
+    public static double distance(double xOne, double yOne, double xTwo, double yTwo ){
+         return  Math.sqrt(Math.pow((xTwo - xOne),2) + Math.pow((yTwo - yOne),2)) ;
+
+
     }
 
     public static void print(String message){
