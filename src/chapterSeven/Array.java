9843895570577752eab90be0d588e954e317c0e0
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2022-01-21 00:32:20 +0100

    Added Array Code

--- src/chapterSeven/Array.java
+++ src/chapterSeven/Array.java
@@ -1,5 +1,7 @@
 package chapterSeven;
 
+import java.util.ArrayList;
+
 public class Array {
     public static void main(String[] args) {
         String[] noisemaker = new String[4];
@@ -14,6 +16,56 @@ public class Array {
         for (int counter = 0; counter< noisemaker.length;counter++ ){
             System.out.println(noisemaker[counter]);
         }
+
+        ArrayList<String> noisemakers = new ArrayList<>();
+        noisemakers.add("Chibuzor");
+        noisemakers.add("Olalekan");
+        noisemakers.add("Tolu");
+        noisemakers.add("Bayo");
+
+        for (int i = 0; i < noisemaker.length; i++){
+            System.out.println(noisemakers.get(i));
+        }
+
+        String[][] noise ={{"Banana", "Orange", "Mangoes"},{"Bread", "Egg","Tomatoes"},{"Rice","Beans","Yam"}};
+        int serialNumber = 0;
+
+        System.out.println("s/n Position\t Objects");
+        for (int column = 0; column < noise.length; column++){
+            for (int row =0; row < 3; row++){
+                serialNumber+= 1;
+
+                System.out.printf("%d\t\t",serialNumber);
+                System.out.printf("%d-%d\t\t", column , row);
+                System.out.print(noise[column][row]+"\n");
+            }
+            System.out.print("\n");
+        }
+
+        serialNumber = 0;
+        System.out.println("s/n Position\t Objects");
+        for (int ro = 2; ro >= 0; ro--){
+            for (int col = 2; col >= 0 ;col--){
+                serialNumber+= 1;
+
+                System.out.printf("%d\t\t",serialNumber);
+                System.out.printf("%d-%d\t\t", ro , col);
+                System.out.print(noise[ro][col]+"\n");
+            }
+        }
+
+        serialNumber = 0;
+        System.out.println("s/n Position\t Objects");
+        for (int ro = 2; ro >= 0; ro--){
+            for (int col = 2; col >= 0 ;col--){
+                serialNumber+= 1;
+
+                System.out.printf("%d\t\t",serialNumber);
+                System.out.printf("%d-%d\t\t", col , ro);
+                System.out.print(noise[col][ro]+"\n");
+            }
+        }
+
 
     }
 }
