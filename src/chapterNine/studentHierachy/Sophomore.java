8a2a3dca3580f467331f37a1e2b29da3ef1c2287
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2022-02-11 14:23:17 +0100

    Updated Code

---
+++ src/chapterNine/studentHierachy/Sophomore.java
@@ -0,0 +1,9 @@
+package chapterNine.studentHierachy;
+
+public class Sophomore extends Undergraduate {
+
+
+    public Sophomore(String lastName, String firstName) {
+        super(lastName, firstName);
+    }
+}
