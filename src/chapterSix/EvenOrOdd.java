9f2e095c3899e2c1feb4afa8ade6c9c85acd7fd2
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2022-02-11 13:46:21 +0100

    Updated Code

--- src/chapterSix/EvenOrOdd.java
+++ src/chapterSix/EvenOrOdd.java
@@ -3,23 +3,42 @@ package chapterSix;
 import java.util.Scanner;
 
 public class EvenOrOdd {
+    private static  Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter A Number Or Any Negative Number To Quit");
-        int userInput = scan.nextInt();
+        int userInput =  input("Enter A Number or Any Negative Number To Quit");
+        checkIfOddOrEven(userInput);
+    }
+
+    private static void checkIfOddOrEven(int userInput) {
         while (userInput >= 0){
           boolean result =   evenOrOdd(userInput);
-            System.out.println(result);
-            System.out.println("Enter A Number Or Any Negative Number To Quit");
-            userInput = scan.nextInt();
+            display(result);
+            display("Enter A Number Or Any Negative Number To Quit");
+            userInput =  input("Enter A Number or Any Negative Number To Quit");
         }
     }
 
-    public static boolean evenOrOdd(int number){
-        if (number % 2 == 0){
+    public static boolean evenOrOdd(int number){  
+        boolean isNumberDivisibleByTwo = number % 2 == 0;
+        if (isNumberDivisibleByTwo){
             return true;
         }
         else
             return false;
+    }
+
+    public static void display(String message){
+        System.out.println(message);
+    }
+    public static void display(int message){
+        System.out.println(message);
+    }
+    public static void display(Boolean message){
+        System.out.println(message);
+    }
+    
+    public static int input(String message){
+        display(message);
+        return scan.nextInt();
     }
 }
--- src/chapterSix/Factors.java
+++ src/chapterSix/Factors.java
@@ -1,35 +1,53 @@
 package chapterSix;
 
-import java.util.IllegalFormatCodePointException;
+import  java.util.IllegalFormatCodePointException;
 import java.util.Scanner;
 
 public class Factors {
+    private static  Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        calculatePrimeNumbers();
+    }
+
     public static int calculateFactorsOf(int userInput){
         int factors = 0;
         for (int number = 1; number <= userInput; number++){
-            if (userInput % number == 0){
-                factors++;
-            }
+            factors = getFactors(userInput, factors, number);
         }
         return factors;
     }
-    public static void main(String[] args) {
-        calculatePrimeNumbers();
 
+    private static int getFactors(int userInput, int factors, int number) {
+       boolean isUserInputAFactorOfNumber = userInput % number == 0;
+        if (isUserInputAFactorOfNumber){
+            factors++;
+        }
+        return factors;
     }
+
     public static void calculatePrimeNumbers(){
-        System.out.println("Enter a number to check for: ");
-        Scanner scan = new Scanner(System.in);
-        int userInput = scan.nextInt();
+        int userInput = input("Enter a number to check for: ");
 
         int result  = calculateFactorsOf(userInput);
 
-        if (result == 2){
+        boolean isResultTwo = result == 2;
+        if (isResultTwo){
             System.out.printf("%d is a Prime number%n",userInput);
         }
         else {
             System.out.printf("%d is not a prime number%n",userInput);
         }
+    }
+
+    public static void display(String message){
+        System.out.println(message);
+    }
+
+    public static int input(String message){
+        display(message);
+        return scan.nextInt();
+
     }
 
 }
