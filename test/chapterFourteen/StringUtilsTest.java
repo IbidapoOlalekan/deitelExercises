65c22d944efc59c0bb8ccabba8bcc7b551fde222
Ibidapo Abdulazeez Olalekan <ibidapoazeez@gmail.com>
2022-03-22 16:42:25 +0100

    Updated Code

---
+++ test/chapterFourteen/StringUtilsTest.java
@@ -0,0 +1,42 @@
+package chapterFourteen;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+    StringUtils stringUtils;
+    @BeforeEach
+    public void setUp(){
+        stringUtils = new StringUtils("Book");
+    }
+    @Test
+    void stringCountTest() {
+       int count =  stringUtils._countOf('o');
+        assertEquals(2, count);
+    }
+
+    @Test
+    void stringIndexOfTest(){
+        int count = stringUtils._indexOf('B');
+        assertEquals(0,count);
+    }
+
+    @Test
+    void stringIndexDoesntExistTest(){
+        int count = stringUtils._indexOf('b');
+        assertEquals(-1,count);
+    }
+    @Test
+    void stringIndexEqualIgnoreCaseTest(){
+        int count = stringUtils._indexOf(true,'b');
+        assertEquals(0,count);
+    }
+    @Test
+    void stringIndexEqualIgnoreCaseFalseTest(){
+        int count = stringUtils._indexOf(false,'b');
+        assertEquals(-1,count);
+    }
+}
